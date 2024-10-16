ba1372a69a29a4dcded67e1285a10ed2251e9c23
cbbsjj <56015193+cbbsjj0314@users.noreply.github.com>
2024-10-17 01:21:07 +0900

    24-10-17 [BJ] 파이썬 차트 제거

--- main_page/urls.py
+++ main_page/urls.py
@@ -11,14 +11,5 @@ urlpatterns = [
 
     #### 범준: python으로 그린 차트 이미지 url
     path('index_test/', views.index_test, name='index_test'),
-    # path('job_postings_count_chart/', views.job_postings_count_chart, name='job_postings_count_chart'),
-    # path('job_postings_count_by_experience_chart/', views.job_postings_count_by_experience_chart, name='job_postings_count_by_experience_chart'),
-    path('entry_tech_stacks_chart/', views.entry_tech_stacks_chart, name='entry_tech_stacks_chart'),
-    path('experienced_tech_stacks_chart/', views.experienced_tech_stacks_chart, name='experienced_tech_stacks_chart'),
-    # path('layered_donut_chart/', views.layered_donut_chart, name='layered_donut_chart'),
-    # path('word_cloud/', views.word_cloud, name='word_cloud'),
-    path('word_cloud_new/', views.word_cloud_new, name='word_cloud_new'),
-    path('word_cloud_experienced/', views.word_cloud_experienced, name='word_cloud_experienced'),
-
     path('chart3_data/', views.chart3_data, name='chart3_data'),
 ]
