6f1c411c3837647365071bd0a3a3a4b3decd5324
ShinDohee <intei0909@gmail.com>
2024-10-17 02:28:03 +0900

    24-10-17 [dh] 1. js 2번/ 4번 차트 그리기

--- main_page/views.py
+++ main_page/views.py
@@ -7,48 +7,14 @@ from math import pi
 from main_page.data_processing import draw_stacked_chart ,save_programming_chart4
 
 
-def main_page(request):
-    # Pandas로 데이터 처리
-    # df = get_programming_data()
-    # chart_data = prepare_chart_data(df)
-    #seaborn 으로 처리
-    chart_path = draw_stacked_chart()
-    save_programming_chart4()
-    # chart_data를 HTML로 전달
-    return render(request, 'main_page/index.html', {'chart_path': chart_path})
-
-def index_test(request):
-    # Pandas로 데이터 처리
-    # df = get_programming_data()
-    # chart_data = prepare_chart_data(df)
-    #seaborn 으로 처리
-    chart_path = draw_stacked_chart()
-    save_programming_chart4()
-    # chart_data를 HTML로 전달
-    return render(request, 'main_page/index.html', {'chart_path': chart_path})
-
-
 matplotlib.use('Agg')  # 이거 없으면 서버 꺼짐
-
 
 def index(request):
     return render(request, 'main_page/index.html')
 
 
-def main_page(request):
-    # Pandas로 데이터 처리
-    # df = get_programming_data()
-    # chart_data = prepare_chart_data(df)
-
-    #seaborn 으로 처리 
-    chart_path = draw_stacked_chart()
-    save_programming_chart4()
-    # chart_data를 HTML로 전달
-    return render(request, 'main_page/index.html', {'chart_path': chart_path})
-
 def index_test(request):
     return render(request, 'main_page/index_test.html')
-
 
 
 from django.http import JsonResponse
