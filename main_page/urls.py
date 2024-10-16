30b7a1c4ccfbe91f877d0ad1b8d5ddbb00198438
ShinDohee <intei0909@gmail.com>
2024-10-17 02:49:16 +0900

    2024-10-17 [dh] 서버 기동시 필요없는 코드 정리 및 py에서 차트 그리는 부분 제거

--- main_page/urls.py
+++ main_page/urls.py
@@ -3,12 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),    
-
-    path('show_chart/', views.main_page, name='show_chart'),
-
-    path('show_chart/', views.main_page, name='show_chart'),
-
-
     #### 범준: python으로 그린 차트 이미지 url
     path('index_test/', views.index_test, name='index_test'),
     path('chart3_data/', views.chart3_data, name='chart3_data'),
--- main_page/views.py
+++ main_page/views.py
@@ -1,24 +1,15 @@
-from django.conf import settings
+
 from django.shortcuts import render
-from main_page.data_processing import draw_stacked_chart ,save_programming_chart4
 import matplotlib
-from math import pi
-
-from main_page.data_processing import draw_stacked_chart ,save_programming_chart4
-
-
-matplotlib.use('Agg')  # 이거 없으면 서버 꺼짐
+from django.http import JsonResponse
+from . import chart3_data_dummy
 
 def index(request):
     return render(request, 'main_page/index.html')
 
-
 def index_test(request):
     return render(request, 'main_page/index_test.html')
 
-
-from django.http import JsonResponse
-from . import chart3_data_dummy
 
 def chart3_data(request):
     return JsonResponse(chart3_data_dummy.data, safe=False)
