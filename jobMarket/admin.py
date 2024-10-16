366b93dbc4819be0235b060a1e4d48f20640bdfd
Dee H Seon <123602432+boolYikes@users.noreply.github.com>
2024-10-16 15:36:17 +0900

    feature/ingestion: Model tweak for admin page readablity, added dummies

--- jobMarket/admin.py
+++ jobMarket/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from jobMarket.models.shared import *
+from jobMarket.models.all_models import *
+from jobMarket.models.misc import *
 
 # Register your models here.
+admin.site.register(Platform)
+admin.site.register(Company)
+admin.site.register(JobPosting)
+admin.site.register(Location)
+admin.site.register(SkillCategory)
+admin.site.register(Skill)
+admin.site.register(Requirement)
+admin.site.register(JobSuperType)
+admin.site.register(JobSubType)
+admin.site.register(JobSuperSubAssociation)
+admin.site.register(RawFile)
