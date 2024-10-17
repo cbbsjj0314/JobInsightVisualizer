e6d03622d773ba8bb357fd8424fbe3e8c37d2d5d
mangodm-web <mangodm.web3@gmail.com>
2024-10-17 17:33:49 +0900

    feat: add PostingByJobPosition model and apply migration to database

--- jobMarket/models/__init__.py
+++ jobMarket/models/__init__.py
@@ -1,6 +1,8 @@
 from .shared import *
+
 # from .job_position import *
 # from .category import *
 from .misc import *
 from .all_models import *
 from .chart4model import *
+from .chart1model import *
