ef02e958054a5a5cedcc48b951f1ffbf4f77ab07
mangodm-web <mangodm.web3@gmail.com>
2024-10-18 02:48:55 +0900

    refactor: improve model validators and verbose names

--- jobMarket/models/chart1model.py
+++ jobMarket/models/chart1model.py
@@ -1,8 +1,11 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 from .shared import BaseInfo
 
 
 class PostingByJobPosition(BaseInfo):
-    collected_on = models.DateTimeField(verbose_name="데이터 수집 일자")
+    collected_on = models.DateTimeField(verbose_name="데이터 수집 일시")
     job_sub_type_name = models.CharField(max_length=100, verbose_name="하위 직군 이름")
-    count = models.IntegerField(verbose_name="공고 수")
+    count = models.IntegerField(
+        verbose_name="공고 수", validators=[MinValueValidator(0)]
+    )
