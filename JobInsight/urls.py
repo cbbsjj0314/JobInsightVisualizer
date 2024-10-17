6fe20cc1e119e4aabf24da72120f561c09d29267
mangodm-web <mangodm.web3@gmail.com>
2024-10-18 02:47:17 +0900

    feat: add Swagger API setup

--- JobInsight/urls.py
+++ JobInsight/urls.py
@@ -14,13 +14,32 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, include
 from django.shortcuts import redirect
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Job Insight Visualizer API",
+        default_version="v1",
+        description="이 API는 채용 공고 데이터를 집계 및 분석하여 채용 트렌드에 대한 인사이트를 제공하기 위한 기반 데이터를 제공합니다.",
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', lambda request: redirect('main_page/', permanent=True)),
-    path('main_page/', include('main_page.urls')),  
-    path('api/stat/', include('jobMarket.urls'))  
+    path("admin/", admin.site.urls),
+    path("", lambda request: redirect("main_page/", permanent=True)),
+    path("main_page/", include("main_page.urls")),
+    path("api/stat/", include("jobMarket.urls")),
+    path(
+        "swagger/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
 ]
